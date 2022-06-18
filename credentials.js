934a2ec57dcb5a979b9637929a736febc6c8b0de
Nguyen Dang <63406002+huudang2840@users.noreply.github.com>
2022-06-18 16:47:07 +0700

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -2,7 +2,7 @@ module.exports = {
     cookieSecret: "your cookie secret goes here",
     mongo: {
         development: {
-            connectionString: 'mongodb://localhost:27017/ViDienTu',
+            connectionString: 'mongodb+srv://huudang284:!23456@cluster0.yxl4h.mongodb.net/?retryWrites=true&w=majority',
         },
         production: {
             connectionString: 'your production_connection_string',
